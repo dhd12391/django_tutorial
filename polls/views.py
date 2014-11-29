fee4bbeb30a577b81183a77431012377bd8c479e
dhd12391 <dhd12391@mail.sfsu.edu>
2014-11-28 19:40:10 -0800

    Part 4: Added form on polls page, allowing user to select a choice and submitting it, which then redirects user to results page

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
-from django.http import HttpResponse
-#from django.template import RequestContext, loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-from polls.models import Question
+from polls.models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -19,4 +18,17 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        #HttpResponseRedirect prevents reposting of data if user hits the back button
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
